d5d252d88a625e0fd77fb3d70af00020d29f8ecb
Ihor <ihor.nazomov@gmail.com>
2021-04-05 03:29:04 +0300

    variable taylor series

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "taylor-git-test"
 
-version := "1.0.0"
+version := "1.0.1"
 
 scalaVersion := "2.13.5"
 
--- src/main/scala/Taylor7.scala
+++ src/main/scala/Taylor7.scala
@@ -7,12 +7,14 @@ import scala.math.pow
  */
 
 object Taylor7 {
-  def apply(x: Double): Double = {
+  def apply(x: Double, n: Int = 4): Double = {
     //step 1: 4 members
     // f(0) = 1/(1-a) = 1
     // f'(0) = 1/(1-a)^2 = 1
     // f"(0) = 2/(1-a)^3 = 2
     // f"'(0) = 6/(1-a)^4 = 6
-    1 + x + x*x + x*x*x
+    Array.range(0, n).foldLeft(0.0) (
+      (z: Double, i: Int) => z + math.pow(x, i) )
+
   }
 }
